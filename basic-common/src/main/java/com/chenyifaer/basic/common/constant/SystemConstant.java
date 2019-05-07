43f919c7c13a8d2d92e7970748a8c27fb5fc9f93
chenyifaer <1044717927@qq.com>
2019-05-07 22:59:31 +0800

    公用模块更新

--- basic-common/src/main/java/com/chenyifaer/basic/common/constant/SystemConstant.java
+++ basic-common/src/main/java/com/chenyifaer/basic/common/constant/SystemConstant.java
@@ -7,8 +7,17 @@ package com.chenyifaer.basic.common.constant;
  */
 public class SystemConstant {
 
-    /****************************************************/
+    /************************后台****************************/
     /** 系统后台用户 - 默认密码 */
     public static final String SYSTEM_PASSWORD = "123456";
+
+
+
+    /************************前台****************************/
+    /** 默认LIMIT查询起始值 */
+    public static final Integer LIMIT_START_SIZE = 0;
+
+    /** 查询商品推荐最大数量 */
+    public static final Integer GOODS_RECOMMENDED_SIZE = 3;
 
 }
